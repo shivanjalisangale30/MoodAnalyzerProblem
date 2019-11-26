2674178b54d0ba8ebd5821f71d493d77d8ea2f4e
shivanjalisangale30 <shivanjalisangale5456@gmail.com>
2019-11-26 10:55:08 +0530

    Given message in constructor should return sad

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -6,16 +6,15 @@ public class MoodAnalyzerTest
     @Test
     public void givenMessage_whenSad_shouldReturnSad()
     {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyseMood("I am in Sad Mood");
-        Assert.assertEquals("Sad", mood);
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+        Assert.assertEquals("Sad",moodAnalyzer.analyseMood());
     }
 
     @Test
     public void givenMessage_whenAnyMood_shouldReturnHappy()
     {
        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-       String mood = moodAnalyzer.analyseMood("I am in Any Mood");
+       String mood = moodAnalyzer.analyseMood();
        Assert.assertEquals("Happy" , mood);
     }
 }
